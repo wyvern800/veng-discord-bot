3c05aa8c198a944725a3477ef48190263a079de1
wyvern800 <matheusguiferreira@hotmail.com>
2019-08-18 23:22:15 -0300

    Additions

--- bot_launcher.py
+++ bot_launcher.py
@@ -1,10 +1,10 @@
 import discord
-from discord.ext import commands, tasks
 import time
 import asyncio
+import random
+from discord.ext import commands, tasks
 
 messages = joined = 0
-
 
 async def atualizar_stats():
     await client.wait_until_ready()
@@ -23,9 +23,16 @@ async def atualizar_stats():
             print(e)
             await asyncio.sleep(5)
 
-Client = discord.Client()
+client = discord.Client()
 client = commands.Bot(command_prefix = '!')
 
+def read_token():
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+
+token = read_token()
 
 # Sends a ready message on_ready() to the console
 @client.event
@@ -41,15 +48,22 @@ async def on_message(message):
     id = client.get_guild(611817575600488449)
     valid_users = ["ferreirA#1058"]
 
+    if message.content.find("!clear") != -1 and str(message.author) in valid_users:
+        await message.channel.send(f'Pronto, eu limpei todas as mensagens!')
+        await message.channel.purge()
+
+    # Comando usado para trocar on nickname instantaneo
     if str(message.channel) == "poste-seu-rsn-aqui":
+            await message.author.edit(nick=message.content)
+            await message.channel.purge(limit=1)
+
+    if str(message.channel) == "converse-com-os-bots":
         if message.content.find("!hello") != -1:
             await message.channel.send("Hi")
         elif message.content == "!usuarios":
             await message.channel.send(f"""Quantidade de Membros {id.member_count}""")
-
-    if str(message.channel) == "poste-seu-rsn-aqui":
-        if message.content == "!help":
-            embed = discord.Embed(title="Help on BOT", description="Some useful commands")
+        elif message.content == "!ajuda":
+            embed = discord.Embed(title="Ajuda com o BOT", description="Some useful commands")
             embed.add_field(name="!hello", value="Greets the user")
             embed.add_field(name="!users", value="Prints number of users")
             await message.channel.send(content=None, embed=embed)
@@ -59,24 +73,29 @@ async def on_message(message):
 async def on_member_join(member):
     global joined
     joined += 1
-
+    responses = [
+        'acabou de mamar piteno',
+        'está na phome de dinheiro e veio pedir esmola pro clan',
+        'viu a oportunidade de brilhar e entrou pro clan']
     for channel in member.guild.channels:
         if str(channel) == "novos-membros":  # We check to make sure we are sending the message in the general channel
-            await channel.send(f'{member.mention}, acabou de entrar no servidor, seja bem vindo!')
+            await channel.send(f'{member.mention}, {random.choice(responses)}, seja bem vindo!')
 
 # Prints when a member lefts a server which has this bot running
 @client.event
 async def on_member_remove(member):
+    global joined
+    joined -= 1
+
     for channel in member.guild.channels:
         if str(channel) == "despedidas":  # We check to make sure we are sending the message in the general channel
             await channel.send(f'{member.mention}, acabou de sair do servidor, hasta la vista!')
-
 
 @client.event # This event runs whenever a user updates: status, game playing, avatar, nickname or role
 async def on_member_update(before, after):
     n = after.nick
     if n: # Check if they updated their username
-        if n.lower().count("theusin") > 0: # If username contains tim
+        if n.lower().count("theusin") or n.lower().count("wenty") > 0: # If username contains tim
             last = before.nick
             if last: # If they had a usernae before change it back to that
                 await after.edit(nick=last)
@@ -84,4 +103,4 @@ async def on_member_update(before, after):
                 await after.edit(nick="Não!")
 
 client.loop.create_task(atualizar_stats())
-client.run('NjEyNDMxNzk4OTk1MjU1MzM3.XVnxrA._mDq7Qa0lb5dlhHbyJEMm-iOIUY')
+client.run(token)
